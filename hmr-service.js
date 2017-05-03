c382d045169db98c1e7303e27be0848586fdb141
cjus <Carlos.Justiniano@gmail.com>
2017-05-03 18:03:36 -0400

    Update to use hydra 1.3.0

--- hmr-service.js
+++ hmr-service.js
@@ -5,30 +5,30 @@
 */
 'use strict';
 
-const version = require('./package.json').version;
-let config = require('fwsp-config');
-
 /**
 * Load configuration file and initialize Hydra.
 */
-config.init('./config/config.json')
-  .then(() => {
-    const hydra = require('hydra');
-    config.version = version;
-    hydra.init(config)
-      .then(() => hydra.registerService())
-      .then((_serviceInfo) => {
-        let logEntry = `Starting ${config.hydra.serviceName} (v.${config.version})`;
-        console.log(logEntry);
-        hydra.on('message', (message) => {
-          hydra.sendReplyMessage(message, {
-            bdy: {
-              message: `Message reply to mid (${message.mid}) by ${config.hydra.serviceName} instance ${hydra.getInstanceID()}`
-            }
-          });
-        });
-      })
-      .catch((err) => {
-        console.log('err', err);
+let config = {};
+const hydra = require('hydra');
+hydra.init(`${__dirname}/config/config.json`, false)
+  .then((newConfig) => {
+    config = newConfig;
+    return hydra.registerService();
+  })
+  .then((serviceInfo) => {
+    let logEntry = `Started ${hydra.getServiceName()} (v.${hydra.getInstanceVersion()})`;
+    console.log(logEntry);
+    console.log(serviceInfo);
+    hydra.on('message', (message) => {
+      hydra.sendReplyMessage(message, {
+        bdy: {
+          message: `Message reply to mid (${message.mid}) by ${config.hydra.serviceName} instance ${hydra.getInstanceID()}`
+        }
       });
+    });
+  })
+  .catch((err) => {
+    console.log('err', err);
+    hydra.shutdown();
+    process.exit(-1);
   });
